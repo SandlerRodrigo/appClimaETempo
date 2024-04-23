b71123012c625c1dded3612aa5a08e15b7bab7fb
Bernardo <b.tarnowski@edu.pucrs.br>
2024-04-23 15:41:15 -0300

    tt

--- components/clock/clock.js
+++ components/clock/clock.js
@@ -1,4 +1,19 @@
-const date = new Date();
-const timestamp = currentDate.getTime();
-const day = getDay();
-const semana = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+function getClockData() {
+    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const d = new Date();
+    let day = weekday[d.getDay()];
+    let mod = document.getElementById("mod");
+    mod.innerHTML = day;
+
+    let prima = d.getHours();
+    let secu = d.getMinutes();
+    let home = document.getElementById("hora");
+    home.innerHTML = prima + ":" + secu;
+
+    let dd = d.getDate();
+    let mm = d.getMonth();
+    let yy = d.getFullYear();
+    let gorge = document.getElementById("omed");
+    gorge.innerHTML = dd + "/" + Number(mm + 1) + "/" + yy;
+
+}
