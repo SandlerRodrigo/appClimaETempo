aa8f73c537fcef80f07463be3cab55ca12cee2ed
Bernardo <b.tarnowski@edu.pucrs.br>
2024-04-23 15:51:14 -0300

    tt

--- components/clock/clock.js
+++ components/clock/clock.js
@@ -14,6 +14,6 @@ function getClockData() {
     let mm = d.getMonth();
     let yy = d.getFullYear();
     let gorge = document.getElementById("omed");
-    gorge.innerHTML = dd + "/" + Number(mm + 1) + "/" + yy;
+    gorge.innerHTML = dd + "/" + Number(mm + 1) + "/" + yy.slice(2);
 
 }
