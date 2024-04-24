6b9d5ba65a95fa34f7a4f5d46ef8dadf3c486e9b
Luca <lucabohneberger@gmail.com>
2024-04-24 15:24:46 -0300

    funcional com imagens

--- components/weeklyTemp/weeklyTemp.js
+++ components/weeklyTemp/weeklyTemp.js
@@ -4,7 +4,7 @@ function getWeeklyTempData(tempo){
     let i = 0
     let num = 0
     while(i < 40){
-        data = new Date(tempo.list[i].dt_txt)
+        let data = new Date(tempo.list[i].dt_txt)
         if(data.getHours()%12 == 0 & data.getHours()!=0 || data.getHours()%21 == 0 & data.getHours()!=0 ){        
             let diaDaSemana = data.toLocaleDateString("en-US", { weekday: "short" })
             document.getElementById("temp" + num).innerHTML = tempo.list[i].main.temp + "°C"
@@ -12,13 +12,29 @@ function getWeeklyTempData(tempo){
             document.getElementById("time" + num).innerHTML = data.getHours()  + "h"
             document.getElementById("rain" + num).innerHTML = tempo.list[i].main.humidity + "%"
             document.getElementById("wind" + num).innerHTML = tempo.list[i].wind.speed + "m/s"
+            document.getElementById("img" + num).src =  "../../images/clear_day.svg"
+            if(tempo.list[i].weather[0].id <= 232 & tempo.list[i].weather[0].id >= 200){
+                document.getElementById("img" + num).src =  "../../images/storm.svg"
+            }else if(tempo.list[i].weather[0].id <= 321 & tempo.list[i].weather[0].id >= 300){
+                document.getElementById("img" + num).src =  "../../images/rain.svg"
+            }else if(tempo.list[i].weather[0].id <= 531 & tempo.list[i].weather[0].id >= 500){
+                document.getElementById("img" + num).src =  "../../images/rain.svg"
+            }else if(tempo.list[i].weather[0].id <= 622 & tempo.list[i].weather[0].id >= 600){
+                document.getElementById("img" + num).src =  "../../images/snow.svg"
+            }else if(tempo.list[i].weather[0].id == 800){
+                document.getElementById("img" + num).src =  "../../images/clear_day.svg"
+            }else if(tempo.list[i].weather[0].id <= 804 & tempo.list[i].weather[0].id > 800){
+                document.getElementById("img" + num).src =  "../../images/cloud.svg"
+            }
             num++
         }
         if(num == 6){
             break;
         }
+        console.log(tempo.list[i].weather[0].id)
         i++
     }
 }
+
 
 
